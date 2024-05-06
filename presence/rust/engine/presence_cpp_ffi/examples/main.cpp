95b32854b9234f3ad7bc26e7c140d287895c97d9
Chen Wang <chenw@google.com>
2024-05-06 21:43:04 +0000

    clean up include for main.cpp.

--- presence/rust/engine/presence_cpp_ffi/examples/main.cpp
+++ presence/rust/engine/presence_cpp_ffi/examples/main.cpp
@@ -12,11 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <iostream>
-#include <thread>
-#include<unistd.h>
-#include <mutex>
 #include <condition_variable>
+#include <mutex>
+#include <thread>
 #include "spdlog/spdlog.h"
 extern "C" {
 #include "presence.h"
