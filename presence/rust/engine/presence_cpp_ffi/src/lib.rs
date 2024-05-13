d3be7da930b00cc6d7bc9f51b680393bbe00c965
Chen Wang <chenw@google.com>
2024-05-13 17:36:54 +0000

    Move extern builder functions from lib.rs to c_to_rust.rs.

--- presence/rust/engine/presence_cpp_ffi/src/lib.rs
+++ presence/rust/engine/presence_cpp_ffi/src/lib.rs
@@ -1,23 +1,15 @@
+// Define Presence Engine APIs to be FFIed to C.
 mod ble_scanner_cpp;
 mod c_to_rust;
 mod discovery_callback_cpp;
 mod rust_to_c;
 
-use c_to_rust::{PresenceBleScanResultBuilder, PresenceDiscoveryRequestBuilder};
-use log::{debug, info};
-
-use presence_core::ble_scan_provider::{
-    BleScanProvider, BleScanner, PresenceScanResult, ScanRequest,
-};
-use presence_core::client_provider::{
-    ClientProvider, DiscoveryCallback, DiscoveryResult, PresenceDiscoveryCondition,
-    PresenceDiscoveryRequest, PresenceIdentityType, PresenceMeasurementAccuracy,
-};
+use presence_core::PresenceEngine;
+use presence_core::ble_scan_provider::PresenceScanResult;
+use presence_core::client_provider::PresenceDiscoveryRequest;
 
 use crate::ble_scanner_cpp::{BleScannerCpp, PresenceStartBleScan};
 use crate::discovery_callback_cpp::{DiscoveryCallbackCpp, PresenceDiscoveryCallback};
-pub use presence_core::client_provider::PresenceMedium;
-use presence_core::PresenceEngine;
 
 #[no_mangle]
 pub unsafe extern "C" fn presence_engine_new(
@@ -54,91 +46,4 @@ pub unsafe extern "C" fn presence_on_scan_result(
     scan_result: *mut PresenceScanResult,
 ) {
     (*engine).on_scan_result(*(Box::from_raw(scan_result)));
-}
-
-#[no_mangle]
-pub extern "C" fn presence_request_builder_new(
-    priority: i32,
-) -> *mut PresenceDiscoveryRequestBuilder {
-    Box::into_raw(Box::new(PresenceDiscoveryRequestBuilder::new(priority)))
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn presence_request_builder_add_condition(
-    builder: *mut PresenceDiscoveryRequestBuilder,
-    action: i32,
-    identity_type: PresenceIdentityType,
-    measurement_accuracy: PresenceMeasurementAccuracy,
-) {
-    (*builder).add_condition(PresenceDiscoveryCondition {
-        action,
-        identity_type,
-        measurement_accuracy,
-    });
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn presence_request_builder_build(
-    builder: *mut PresenceDiscoveryRequestBuilder,
-) -> *mut PresenceDiscoveryRequest {
-    Box::into_raw(Box::new(Box::from_raw(builder).build()))
-}
-
-#[no_mangle]
-pub extern "C" fn presence_ble_scan_result_builder_new(
-    medium: PresenceMedium,
-) -> *mut PresenceBleScanResultBuilder {
-    Box::into_raw(Box::new(PresenceBleScanResultBuilder::new(medium)))
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn presence_ble_scan_result_builder_add_action(
-    builder: *mut PresenceBleScanResultBuilder,
-    action: i32,
-) {
-    (*builder).add_action(action);
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn presence_ble_scan_result_builder_build(
-    builder: *mut PresenceBleScanResultBuilder,
-) -> *mut PresenceScanResult {
-    Box::into_raw(Box::new(Box::from_raw(builder).build()))
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn presence_request_debug_print(request: *const PresenceDiscoveryRequest) {
-    println!("Rust FFI Lib: {:?}", *request);
-}
-
-#[no_mangle]
-pub extern "C" fn presence_enum_medium_debug_print(presence_medium: PresenceMedium) {
-    debug!("Medium type: {:?}", presence_medium)
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{
-        presence_request_builder_add_condition, presence_request_builder_build,
-        presence_request_builder_new,
-    };
-    use presence_core::client_provider::PresenceIdentityType;
-    use presence_core::client_provider::PresenceMeasurementAccuracy;
-
-    #[test]
-    fn test_request_builder() {
-        unsafe {
-            let builder = presence_request_builder_new(1);
-            presence_request_builder_add_condition(
-                builder,
-                10,
-                PresenceIdentityType::Private,
-                PresenceMeasurementAccuracy::BestAvailable,
-            );
-            let request = presence_request_builder_build(builder);
-            assert_eq!((*request).priority, 1);
-            assert_eq!((*request).conditions.len(), 1);
-            assert_eq!((*request).conditions[0].action, 10);
-        }
-    }
 }
