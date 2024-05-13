d1ff1d810445b606c7484bc0cd094accd47e50c4
Chen Wang <chenw@google.com>
2024-05-13 17:48:16 +0000

    Rename to c_to_rust for all C to FFI.

--- presence/rust/engine/presence_cpp_ffi/cpp/c_to_rust.cpp
+++ presence/rust/engine/presence_cpp_ffi/cpp/c_to_rust.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../presence.h"
-#include "data.hpp"
+#include "c_to_rust.hpp"
 
 struct PresenceBleScanRequest* presence_ble_scan_request_new(int priority) {
   PresenceBleScanRequest* request = new PresenceBleScanRequest();
