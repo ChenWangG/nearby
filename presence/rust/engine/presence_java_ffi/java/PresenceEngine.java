7e9ea1f606d0d3a9d67081f789bf06c92e7b3657
Chen Wang <chenw@google.com>
2024-05-20 17:37:12 +0000

    Add callback from Rust to Java.

--- presence/rust/engine/presence_java_ffi/java/PresenceEngine.java
+++ presence/rust/engine/presence_java_ffi/java/PresenceEngine.java
@@ -1,22 +1,41 @@
 package com.google.nearby.presence.engine;
+
 // To generate JNI header, run
 //    javac -h . PresenceEngine.java
 public class PresenceEngine {
 
-    static {
-        System.loadLibrary("presence_java");
-    }
-
-   // No need to pass callbacks.
-    public static native long PresenceEngineNew();
-    public static native void PresenceEngineDebug(long engine);
-
-  public PresenceEngine() {
-    rust_engine_ptr = PresenceEngineNew();
+  static {
+    System.loadLibrary("presence_java");
   }
 
-  public void Debug() {
-    PresenceEngineDebug(rust_engine_ptr);
+  // No need to pass callbacks.
+  private static native long presenceEngineNew();
+
+  private static native long presenceEngineRun(long engine, PresenceEngine callbacks);
+
+  private static native void presenceEngineDebug(long engine);
+
+  private static native void presenceEngineFree(long engine);
+
+  public void onDiscovery(int res) {
+    System.out.println("onDiscovery: res = " + res);
+  }
+
+  public PresenceEngine() {
+    rust_engine_ptr = presenceEngineNew();
+  }
+
+  public void run() {
+    presenceEngineRun(rust_engine_ptr, this);
+  }
+
+  public void free() {
+    presenceEngineFree(rust_engine_ptr);
+  }
+
+  public void debug() {
+    presenceEngineDebug(rust_engine_ptr);
+
   }
 
   // Memory address of Rust Engine.
--- presence/rust/engine/presence_java_ffi/src/lib.rs
+++ presence/rust/engine/presence_java_ffi/src/lib.rs
@@ -2,21 +2,26 @@
 //!
 extern crate jni;
 
-use jni::objects::{JClass, JString};
-use jni::sys::jlong;
+use jni::objects::{JClass, JObject};
+use jni::sys::{jint, jlong};
 use jni::JNIEnv;
 
 pub struct PresenceTestEngine {
     id: i32,
 }
 
+impl PresenceTestEngine {
+    pub fn run(&mut self) {
+        println!("Engine run!")
+    }
+}
+
 /// JNI bindings for `PresenceEngineNew` method in `com.google.nearby.presence.engine`.
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn Java_com_google_nearby_presence_engine_PresenceEngine_PresenceEngineNew(
+pub extern "system" fn Java_com_google_nearby_presence_engine_PresenceEngine_presenceEngineNew(
     _env: JNIEnv,
     _class: JClass,
-    _input: JString,
 ) -> jlong {
     let engine = PresenceTestEngine { id: 101 };
     Box::into_raw(Box::new(engine)) as jlong
@@ -24,7 +29,25 @@ pub extern "system" fn Java_com_google_nearby_presence_engine_PresenceEngine_Pre
 
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn Java_com_google_nearby_presence_engine_PresenceEngine_PresenceEngineDebug(
+pub extern "system" fn Java_com_google_nearby_presence_engine_PresenceEngine_presenceEngineRun(
+    env: JNIEnv,
+    _class: JClass,
+    engine: jlong,
+    callbacks: JObject,
+) {
+    let res: jint = 32;
+    env.call_method(callbacks, "onDiscovery", "(I)V", &[res.into()])
+        .unwrap();
+    unsafe {
+        let engine_ptr = engine as *mut PresenceTestEngine;
+        (*engine_ptr).run();
+        (*engine_ptr).run();
+    }
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "system" fn Java_com_google_nearby_presence_engine_PresenceEngine_presenceEngineDebug(
     _env: JNIEnv,
     _class: JClass,
     engine: jlong,
@@ -32,5 +55,17 @@ pub extern "system" fn Java_com_google_nearby_presence_engine_PresenceEngine_Pre
     unsafe {
         let engine_ptr = engine as *mut PresenceTestEngine;
         println!("Engine id {}", (*engine_ptr).id);
+    }
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "system" fn Java_com_google_nearby_presence_engine_PresenceEngine_presenceEngineFree(
+    _env: JNIEnv,
+    _class: JClass,
+    engine: jlong,
+) {
+    unsafe {
+        let _ = Box::from_raw(engine as *mut PresenceTestEngine);
     }
 }
