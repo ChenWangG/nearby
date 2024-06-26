da02942b96efb5096538ff9b6497d5742c9f8b75
Chen Wang <chenw@google.com>
2024-06-26 07:41:33 +0000

    Add TestApi and load libtest_api.so.

--- presence/rust/system_api/android/examples/Main.java
+++ presence/rust/system_api/android/examples/Main.java
@@ -1,5 +1,9 @@
+import com.google.nearby.api.TestApi;
+
 public class Main {
   public static void main(String[] args) {
+    TestApi test_api = new TestApi();
+    test_api.onScanResult();
     System.out.println("Example to demo Nearby Android System API in Rust.");
   }
 }
---
+++ presence/rust/system_api/android/examples/TestApi.java
@@ -0,0 +1,21 @@
+package com.google.nearby.api;
+
+// To generate JNI header, run
+//    javac -h . *.java
+// To get method signatures:
+//    javac *.java && javap -s *.class
+
+public class TestApi {
+
+  static {
+    System.loadLibrary("test_api");
+  }
+
+  /* ========== Native methods implemented in Rust. ========== */
+  private static native void start_scan();
+
+  /* ================= Called from Rust. ==================== */
+  public void onScanResult() {
+    System.out.println("onStart.");
+  }
+}
