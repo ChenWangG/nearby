cc1a77d427c9659e7a180bb3954654f8c2a603d3
Chen Wang <chenw@google.com>
2024-10-25 23:35:51 +0000

    Separate Engine and EventProcessor<EngineProcessor>

--- presence/rust/engine/presence_rust/src/engine.rs
+++ presence/rust/engine/presence_rust/src/engine.rs
@@ -1,5 +1,32 @@
-use crate::DiscoveryCallback;
-use event_poller::EventProcessor;
+use crate::{async_block_on, DiscoveryCallback, DiscoveryResult};
+use event_poller::{EventPoller, EventProcessor, EventWriter};
+
+pub fn create<C>(callback: C) -> (Engine, EventPoller<EngineProcessor<C>>)
+where
+    C: DiscoveryCallback + Send + 'static,
+{
+    let (engine_writer, engine_poller) = event_poller::create(EngineProcessor::new(callback));
+    (Engine::new(engine_writer), engine_poller)
+
+}
+
+pub struct Engine {
+    writer: EventWriter<EngineEvent>,
+}
+
+impl Engine {
+    pub fn new(writer: EventWriter<EngineEvent>) -> Self {
+        Self{ writer }
+    }
+    pub async fn set_request(&mut self, request: DiscoveryResult) {
+        self.writer.write(EngineEvent::Ble).await.unwrap();
+    }
+
+    pub async fn stop(&mut self) {
+        self.writer.stop().await.unwrap();
+    }
+}
+
 
 #[derive(Clone)]
 pub enum EngineEvent {
