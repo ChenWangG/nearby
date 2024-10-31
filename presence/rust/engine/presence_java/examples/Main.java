0e33cbfcfb73ffec02397bd3bba16d1e4ec97640
Chen Wang <chenw@google.com>
2024-10-31 17:53:23 +0000

    Hook Presence Java lib to examples/Main.

--- presence/rust/engine/presence_java/examples/Main.java
+++ presence/rust/engine/presence_java/examples/Main.java
@@ -1,7 +1,12 @@
+import com.google.nearby.presence.Presence;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("==========================================================");
     System.out.println("======== Example to demo Presence Rust Java API.==========");
     System.out.println("==========================================================");
+    Presence presence = new Presence();
+    presence.hello();
+    System.out.println("==========================================================");
+    System.out.println("========== End of demo Presence Rust Java API.============");
   }
 }
--- presence/rust/engine/presence_java/java/Presence.java
+++ presence/rust/engine/presence_java/java/Presence.java
@@ -2,6 +2,7 @@
 //    javac -h . *.java
 // To get method signatures:
 //    javac *.java && javap -s *.class
+package com.google.nearby.presence;
 
 // Presence in Java Wrapping the Rust implementation.
 public class Presence {
@@ -10,9 +11,15 @@ public class Presence {
     System.loadLibrary("presence_java");
   }
 
-   /* ========== Native methods implemented in Rust. ========== */
-    private static native long newPresence();
-    private static native void setRequest(long presence, long request);
+  /* ========== Native methods implemented in Rust. ========== */
+  private static native long newPresence();
+  private static native void setRequest(long presence, long request);
+
+  public void hello() {
+      System.out.println("Hello from Presence Java Lib.");
+  }
+
+
 
   // Memory address of Rust Presence.
   // Opaque pointer to be passed back and forth between Rust and Java.
