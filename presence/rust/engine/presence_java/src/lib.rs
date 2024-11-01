6274c6bef8ae4e14b0921e787f820cda55bc1647
Chen Wang <chenw@google.com>
2024-11-01 17:53:49 +0000

    Use GlobaRef to keep pesence_java reference in Rust.
    And callback to Java with the globalRef.

--- presence/rust/engine/presence_java/src/lib.rs
+++ presence/rust/engine/presence_java/src/lib.rs
@@ -1,5 +1,5 @@
 use std::sync::mpsc;
-use jni::objects::{JClass, JObject, JValue};
+use jni::objects::{GlobalRef, JClass, JObject, JValue};
 use jni::sys::jlong;
 use jni::{JNIEnv, JavaVM};
 use presence_rust::client::{Client, DiscoveryCallback, DiscoveryResult};
@@ -7,7 +7,10 @@ use presence_rust::Runtime;
 
 static ON_DISCOVERY_SIGNATURE: &str = "(J)V";
 
-struct Callback {}
+struct Callback {
+    jvm: JavaVM,
+    presence_java: GlobalRef,
+}
 
 impl DiscoveryCallback for Callback {
     fn on_update(&self, result: DiscoveryResult) {
@@ -22,6 +25,12 @@ struct PresenceRust {
 }
 
 impl PresenceRust {
+    pub fn client(&mut self) -> &mut Client {
+        &mut self.client
+    }
+    pub fn runtime(&mut self) -> &mut Runtime<Callback> {
+        &mut self.runtime
+    }
     pub fn test_ptr(&self) {
        println!("PresentRust test ptr.");
     }
@@ -32,8 +41,7 @@ impl PresenceRust {
 pub unsafe extern "system" fn Java_com_google_nearby_presence_Presence_newPresence
 (_env: JNIEnv,
  _class: JClass) -> jlong {
-    let callback = Callback {};
-    let (client, runtime) = presence_rust::create(callback);
+    let (client, runtime) = presence_rust::create();
     let presence_rust = PresenceRust{client, runtime};
     Box::into_raw(Box::new(presence_rust)) as jlong
 }
@@ -47,12 +55,21 @@ pub unsafe extern "system" fn Java_com_google_nearby_presence_Presence_start
 ) {
     println!("Presence Rust start.");
 
+    let callback = Callback{
+        jvm: env.get_java_vm().unwrap(),
+        presence_java: env.new_global_ref(presence_java).unwrap(),
+    };
+
+    /*
     let presence_rust_ptr = presence_rust_ptr as *mut PresenceRust;
-    (*presence_rust_ptr).test_ptr();
+    (*presence_rust_ptr).runtime().set_discovery_callback(callback);
+     */
+
+    // let mut presence_java = env.new_global_ref(presence_java).unwrap();
 
     let addr = 1 as jlong;
     env.call_method(
-        presence_java,
+        callback.presence_java.as_obj(),
         "onDiscovery",
         ON_DISCOVERY_SIGNATURE,
         &[addr.into()],
