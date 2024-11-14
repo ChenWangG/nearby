6ba2c524beec82864c4f1a459c5948e63361d9f5
Chen Wang <chenw@google.com>
2024-11-14 22:51:15 +0000

    Callback from BleWRapper in a separated thread.

--- presence/rust/ble/java/unit_test/BleWrapper.java
+++ presence/rust/ble/java/unit_test/BleWrapper.java
@@ -1,5 +1,7 @@
 package com.google.nearby.ble;
 
+import java.lang.Thread;
+
 public class BleWrapper {
   public interface ScanCallback {
     public void onScanResult (int callbackType,
@@ -10,6 +12,12 @@ public class BleWrapper {
 
   public void startScan(ScanCallback callback) {
     System.out.println("[Java][BleWrapper] startScan");
+    new Thread() {
+      public void run() {
+        System.out.println("[Java][BleWrapper] startScan callback.");
+        callback.onScanResult(1, new ScanResult());
+      }
+    }.start();
   }
 
   public void stopScan(long callbackPtr) {
