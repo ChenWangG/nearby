300578bdc729ebbb27d971206795d72decc1fdd9
Chen Wang <chenw@google.com>
2024-11-14 01:28:19 +0000

    Rename TestBle.java to Main.java and move to unit_test.

--- presence/rust/ble/java/unit_test/Main.java
+++ presence/rust/ble/java/unit_test/Main.java
@@ -1,6 +1,6 @@
 import com.google.nearby.ble_test.TestBleScanner;
 
-public class TestBle {
+public class Main {
   public static void main(String[] args) {
     TestBleScanner test_scanner = new TestBleScanner();
     System.out.println("======== Test BLE Java.==========");
