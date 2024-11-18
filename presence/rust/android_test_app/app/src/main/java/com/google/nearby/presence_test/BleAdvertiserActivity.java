05506795c23a3876acd378b31f1fe7c98b6c9974
Chen Wang <chenw@google.com>
2024-11-18 23:44:29 +0000

    Start BLE advetiser.

--- presence/rust/android_test_app/app/src/main/java/com/google/nearby/presence_test/BleAdvertiserActivity.java
+++ presence/rust/android_test_app/app/src/main/java/com/google/nearby/presence_test/BleAdvertiserActivity.java
@@ -1,4 +1,5 @@
 package com.google.nearby.presence_test;
+import com.google.nearby.ble.Constants;
 
 import static com.google.nearby.presence_test.MainActivity.TAG;
 
@@ -7,6 +8,7 @@ import android.bluetooth.le.AdvertiseCallback;
 import android.bluetooth.le.AdvertiseData;
 import android.bluetooth.le.AdvertiseSettings;
 import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.os.ParcelUuid;
 import android.util.Log;
 import androidx.annotation.Nullable;
 
@@ -33,28 +35,37 @@ public class BleAdvertiserActivity extends AdvertiserActivity {
     if (leAdvertiser != null) {
       AdvertiseSettings settings =
           new AdvertiseSettings.Builder()
-              .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
-              .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
-              .setConnectable(true)
+              .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER)
+              .setConnectable(false)
               .build();
 
-      AdvertiseData advertiseData = null;
+      AdvertiseData advertiseData =  new AdvertiseData.Builder()
+          .addServiceData(Constants.PRESENCE_SERVICE_DATA_UUID, getServiceData())
+          .build();
 
-      AdvertiseData scanResponse = null;
-      /*
+      AdvertiseData scanResponse = new AdvertiseData.Builder().build();
       try {
         leAdvertiser.startAdvertising(
             settings, advertiseData, scanResponse, new AdvertiseCallback() {
               @Override
               public void onStartSuccess(AdvertiseSettings settingsInEffect) {
                 super.onStartSuccess(settingsInEffect);
-                log("Succeeded to start advertising.");
+                Log.i(TAG, "Succeeded to start advertising.");
+                // TODO: hook log to AdvertiserActivity.
+                // log("Succeeded to start advertising.");
               }
             });
       } catch (NullPointerException | IllegalStateException | SecurityException e) {
         Log.e(TAG, "Failed to start broadcast with Exception: " + e.toString());
       }
-       */
     }
+  }
+
+  private byte[] getServiceData() {
+    return new byte[] {
+        0b00000001,
+        0b00000010,
+        0b00000011,
+    };
   }
 }
