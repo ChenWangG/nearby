95f49418c92530f510b29ccdebee5c7260ea2f73
Chen Wang <chenw@google.com>
2024-11-19 17:53:19 +0000

    Debug print ScanResult FFI.

--- presence/rust/ble/src/scan_result.rs
+++ presence/rust/ble/src/scan_result.rs
@@ -2,7 +2,7 @@ use jni::JNIEnv;
 use jni::objects::JClass;
 use jni::objects::JByteArray;
 use jni::sys::jlong;
-use log::info;
+use log::{debug, info};
 use crate::{BleScanResult, ScanCallbackBox};
 use jni::objects::ReleaseMode;
 
@@ -19,7 +19,10 @@ pub unsafe extern "system" fn Java_com_google_nearby_ble_ScanResult_toRustScanRe
 ) -> jlong {
     info!("To Rust ScanResult.");
     let elements = env.get_array_elements(&service_data, ReleaseMode::NoCopyBack).unwrap();
-    let elem0 = elements.get(0).unwrap() as &i8;
-    println!("elem0 is {}", elem0);
+    let element_vec = elements.to_vec();
+    debug!("Elements length: {}",  element_vec.len());
+    for element in element_vec {
+        debug!("{}", element as i8);
+    }
     0
 }
