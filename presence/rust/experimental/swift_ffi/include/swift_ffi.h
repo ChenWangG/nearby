290a95cb9f93d5c5c066d2961b1e3f98dadccd6e
Chen Wang <ChenWangG@google.com>
2024-12-02 11:07:27 -0800

    Add callback type to call from Rust to Swift.

--- presence/rust/experimental/swift_ffi/include/swift_ffi.h
+++ presence/rust/experimental/swift_ffi/include/swift_ffi.h
@@ -3,4 +3,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+typedef void (*CallSwift)(int32_t);
+
 int32_t test_swift_ffi(void);
+
+int32_t test_swift_callback(CallSwift call_swift);
--- presence/rust/experimental/swift_ffi/src/lib.rs
+++ presence/rust/experimental/swift_ffi/src/lib.rs
@@ -1,5 +1,14 @@
+pub type CallSwift = fn(i32);
+
 #[no_mangle]
 pub extern fn test_swift_ffi() -> i32 {
   println!("Test Swift FFI");
   17
+}
+
+#[no_mangle]
+pub unsafe extern fn test_swift_callback(call_swift: CallSwift) -> i32 {
+  println!("Test Swift FFI");
+  call_swift(3);
+  18
 }
