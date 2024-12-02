8e72bc2fb29e28c7415e899d4c20d6446d91ac42
Chen Wang <chenw@google.com>
2024-12-02 22:06:08 +0000

    Split out android impl for BLE.

---
+++ presence/rust/ble/src/android.rs
@@ -0,0 +1,68 @@
+use crate::scan_result::ScanResult;
+use crate::{
+    BleScanRequest, BleScanResult, BleScanner, ScanCallback, ScanCallbackBox, Scanner, BLE_CLASS,
+    BUILD_SIGNATURE, START_SIGNATURE,
+};
+use jni::objects::{GlobalRef, JClass};
+use jni::sys::jlong;
+use jni::{JNIEnv, JavaVM};
+use log::{debug, info};
+
+impl Scanner for BleScanner {
+    fn start(&self, request: BleScanRequest, callback: impl ScanCallback) {
+        info!("(PresenceRust) BleScanner start.");
+        let scan_callback_box = ScanCallbackBox {
+            scan_callback: Box::new(callback),
+        };
+        let callback_ptr = Box::into_raw(Box::new(scan_callback_box)) as jlong;
+        info!("BleScanner Lib start with callback addr: {}.", callback_ptr);
+        let mut env = self.jvm.get_env().unwrap();
+        env.call_method(
+            self.java_ble_scanner.as_obj(),
+            "start",
+            START_SIGNATURE,
+            &[callback_ptr.into()],
+        )
+        .unwrap();
+    }
+}
+
+impl BleScanner {
+    pub fn new(jvm: JavaVM) -> Self {
+        let mut env = jvm.get_env().unwrap();
+        let ble_scanner = env
+            .call_static_method(BLE_CLASS, "build", BUILD_SIGNATURE, &[])
+            .unwrap()
+            .l()
+            .unwrap();
+        let java_ble_scanner = env.new_global_ref(ble_scanner).unwrap();
+        Self {
+            jvm,
+            java_ble_scanner,
+        }
+    }
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub unsafe extern "system" fn Java_com_google_nearby_ble_BleScanner_onScanResult(
+    mut env: JNIEnv,
+    _class: JClass,
+    callback_ptr: jlong,
+    result: jlong,
+) {
+    info!(
+        "(PresenceRust) BleScanner_onScanResult with callback_ptr: {}.",
+        callback_ptr
+    );
+    let scan_callback_box_ptr = callback_ptr as *mut ScanCallbackBox;
+    let result_ptr = result as *mut ScanResult;
+    let service_data = (&*result_ptr).service_data().clone();
+    debug!("BleScanner onScanResult:");
+    for data in &service_data {
+        debug!("{}", data)
+    }
+    &(*scan_callback_box_ptr)
+        .scan_callback
+        .on_update(BleScanResult::new(1, service_data));
+}
--- presence/rust/ble/src/lib.rs
+++ presence/rust/ble/src/lib.rs
@@ -1,14 +1,11 @@
+mod android;
 mod scan_result;
 
-use jni::{JNIEnv, JavaVM};
-use jni::objects::{GlobalRef, JClass, JObject};
-use jni::sys::jlong;
-use log::{debug, info};
-use crate::scan_result::ScanResult;
+use jni::objects::GlobalRef;
+use jni::{JavaVM};
 
 static BLE_CLASS: &str = "com/google/nearby/ble/BleScanner";
-static BUILD_SIGNATURE: &str =
-    "()Lcom/google/nearby/ble/BleScanner;";
+static BUILD_SIGNATURE: &str = "()Lcom/google/nearby/ble/BleScanner;";
 static START_SIGNATURE: &str = "(J)V";
 pub struct BleScanRequest {
     uuid: String,
@@ -17,7 +14,7 @@ pub struct BleScanRequest {
 
 impl BleScanRequest {
     pub fn new(uuid: String, priority: i32) -> Self {
-       BleScanRequest{ uuid,  priority }
+        BleScanRequest { uuid, priority }
     }
 
     pub fn uuid(&self) -> &str {
@@ -35,7 +32,10 @@ pub struct BleScanResult {
 
 impl BleScanResult {
     pub fn new(tx_power: i32, service_data: Vec<u8>) -> Self {
-        BleScanResult { tx_power, service_data }
+        BleScanResult {
+            tx_power,
+            service_data,
+        }
     }
 
     pub fn tx_power(&self) -> i32 {
@@ -50,9 +50,9 @@ impl BleScanResult {
 // TODO: avoid static lifetime? This is eventually owned by system API.
 // Pass type T: ScanCallback instead of impl ScanCallback.
 pub trait ScanCallback: std::marker::Sync + 'static {
-  fn on_update(&self, result: BleScanResult);
+    fn on_update(&self, result: BleScanResult);
 }
-pub trait Scanner : Send + 'static {
+pub trait Scanner: Send + 'static {
     fn start(&self, request: BleScanRequest, callback: impl ScanCallback);
 }
 
@@ -63,57 +63,4 @@ pub struct ScanCallbackBox {
 pub struct BleScanner {
     jvm: JavaVM,
     java_ble_scanner: GlobalRef,
-}
-
-impl Scanner for BleScanner {
-    fn start(&self, request: BleScanRequest, callback: impl ScanCallback) {
-        info!("(PresenceRust) BleScanner start.");
-        let scan_callback_box = ScanCallbackBox { scan_callback: Box::new(callback)};
-        let callback_ptr = Box::into_raw(Box::new(scan_callback_box)) as jlong;
-        info!("BleScanner Lib start with callback addr: {}.", callback_ptr);
-        let mut env = self.jvm.get_env().unwrap();
-        env.call_method(
-            self.java_ble_scanner.as_obj(),
-            "start",
-            START_SIGNATURE,
-            &[callback_ptr.into()],
-        ).unwrap();
-    }
-}
-
-impl BleScanner {
-    pub fn new(jvm: JavaVM) -> Self {
-        let mut env = jvm.get_env().unwrap();
-        let ble_scanner = env.call_static_method(
-            BLE_CLASS,
-            "build",
-            BUILD_SIGNATURE,
-            &[],
-        )
-            .unwrap()
-            .l()
-            .unwrap();
-        let java_ble_scanner =env.new_global_ref(ble_scanner).unwrap();
-        Self { jvm, java_ble_scanner }
-
-    }
-}
-
-#[no_mangle]
-#[allow(non_snake_case)]
-pub unsafe extern "system" fn Java_com_google_nearby_ble_BleScanner_onScanResult
-(mut env: JNIEnv,
- _class: JClass,
- callback_ptr: jlong,
- result: jlong,
-) {
-    info!("(PresenceRust) BleScanner_onScanResult with callback_ptr: {}.", callback_ptr);
-    let scan_callback_box_ptr = callback_ptr as *mut ScanCallbackBox;
-    let result_ptr = result as *mut ScanResult;
-    let service_data = (&*result_ptr).service_data().clone();
-    debug!("BleScanner onScanResult:");
-    for data in &service_data {
-       debug!("{}", data)
-    }
-    &(*scan_callback_box_ptr).scan_callback.on_update(BleScanResult::new(1, service_data));
 }
