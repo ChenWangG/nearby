b4e919b5e86be18a4fadeec7335e0053767bfe2f
Chen Wang <ChenWangG@google.com>
2024-12-02 12:34:48 -0800

    Enable OSLogger after toggle on metadata in log viewer.

--- presence/rust/experimental/swift_ffi/src/lib.rs
+++ presence/rust/experimental/swift_ffi/src/lib.rs
@@ -14,13 +14,12 @@ pub extern fn rust_object_new(call_swift: CallSwift) -> *mut RustObject {
   // The log level is enabled in iOS by set env variable
   // RUST_LOG=debug
   // in XCode Product -> Scheme -> Edit Scheme
-  env_logger::init();
-  /* TODO: re-enable after the log output shows module name.
-  OsLogger::new("com.example.test")
+  // env_logger::init();
+  // TODO toggle on metadata in Xcode log view to show the log source.
+  OsLogger::new("com.google.SwiftFFI")
       .level_filter(LevelFilter::Debug)
       .init()
       .unwrap();
-   */
   info!("new Rust Object.");
   Box::into_raw(Box::new(RustObject { call_swift }))
 }
@@ -28,7 +27,7 @@ pub extern fn rust_object_new(call_swift: CallSwift) -> *mut RustObject {
 #[no_mangle]
 pub unsafe extern "C" fn rust_object_call_swift(rust_object: *mut RustObject) {
   debug!("Rust object call swift.");
-  ((*rust_object).call_swift)(2);
+  ((*rust_object).call_swift)(3);
 }
 
 #[no_mangle]
