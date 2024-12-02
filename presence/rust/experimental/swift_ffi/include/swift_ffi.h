f41ee5c4dfad88a33e739165fbaeac6d35037144
Chen Wang <ChenWangG@google.com>
2024-12-02 11:38:46 -0800

    Clean up Rust/Swift interop.

--- presence/rust/experimental/swift_ffi/include/swift_ffi.h
+++ presence/rust/experimental/swift_ffi/include/swift_ffi.h
@@ -3,8 +3,12 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+typedef struct RustObject RustObject;
+
 typedef void (*CallSwift)(int32_t);
 
-int32_t test_swift_ffi(void);
+struct RustObject *rust_object_new(CallSwift call_swift);
+
+void rust_object_call_swift(struct RustObject *rust_object);
 
 int32_t test_swift_callback(CallSwift call_swift);
--- presence/rust/experimental/swift_ffi/src/lib.rs
+++ presence/rust/experimental/swift_ffi/src/lib.rs
@@ -1,9 +1,19 @@
 pub type CallSwift = fn(i32);
 
+pub struct RustObject {
+    call_swift: CallSwift,
+}
+
 #[no_mangle]
-pub extern fn test_swift_ffi() -> i32 {
-  println!("Test Swift FFI");
-  17
+pub extern fn rust_object_new(call_swift: CallSwift) -> *mut RustObject {
+  println!("[Rust] new Rust Object");
+  Box::into_raw(Box::new(RustObject { call_swift }))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn rust_object_call_swift(rust_object: *mut RustObject) {
+  println!("[Rust] Rust object call swift.");
+  ((*rust_object).call_swift)(2);
 }
 
 #[no_mangle]
