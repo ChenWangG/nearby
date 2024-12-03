ee6aac81bc3707c6ad9f585ae25bd1bc4b57dead
Chen Wang <ChenWangG@google.com>
2024-12-03 10:27:59 -0800

    Add ios_test_app.

--- presence/rust/ble_test/src/test_swift_ble_scanner.rs
+++ presence/rust/ble_test/src/test_swift_ble_scanner.rs
@@ -4,5 +4,5 @@ use ble::SwiftStartBleScan;
 
 #[no_mangle]
 pub extern fn ble_scanner_new(swift_start_ble_scan: SwiftStartBleScan) -> *mut BleScanner {
-    null_mut()
+    Box::into_raw(Box::new(BleScanner::new(swift_start_ble_scan)))
 }
